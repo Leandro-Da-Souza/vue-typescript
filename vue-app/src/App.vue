befe597db28fbbe7daab0c6baf76eab533cb43b7
Leandro Da Souza <dasouza.leandro@gmail.com>
2021-08-04 09:06:01 +0200

    add vue router to project

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -1,20 +1,12 @@
 <template>
   <div id="app">
-    <Character />
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
-
-<script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import Character from './components/Character.vue';
-
-@Component({
-  components: {
-    Character
-  },
-})
-export default class App extends Vue {}
-</script>
 
 <style>
 #app {
@@ -23,6 +15,18 @@ export default class App extends Vue {}
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
