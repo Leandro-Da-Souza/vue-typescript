e64d96fc617a58c4b1e42d641ba3b5574526b152
Leandro Da Souza <dasouza.leandro@gmail.com>
2021-08-04 10:51:12 +0200

    add appropriate routes

---
+++ vue-app/src/views/Inventory.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="inventory">
+      hello
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Inventory extends Vue {
+
+}
+</script>
+
+
+<style>
+
+</style>
---
+++ vue-app/src/views/Shop.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="shop">
+      <p>{{firstName}}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue} from 'vue-property-decorator'
+export default class Shop extends Vue {
+    firstName = 'Leo'
+}
+</script>
+
+<style>
+
+</style>
